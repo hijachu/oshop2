9e366897b84e63af2bfc8b07c38e269d3603a33d
YuChih Hu <hijac.hu@gmail.com>
2020-04-07 13:47:53 +0800

    Add angular/fire and firebase module. hack for tsconfig(skiplibcheck)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDw95Nk8L56vSgLRWCvHft7umDjh-Q-wZw",
+    authDomain: "oshop2-a0e4e.firebaseapp.com",
+    databaseURL: "https://oshop2-a0e4e.firebaseio.com",
+    projectId: "oshop2-a0e4e",
+    storageBucket: "oshop2-a0e4e.appspot.com",
+    messagingSenderId: "758576304640",
+    appId: "1:758576304640:web:38c9ab697d338dd5407ee5"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDw95Nk8L56vSgLRWCvHft7umDjh-Q-wZw",
+    authDomain: "oshop2-a0e4e.firebaseapp.com",
+    databaseURL: "https://oshop2-a0e4e.firebaseio.com",
+    projectId: "oshop2-a0e4e",
+    storageBucket: "oshop2-a0e4e.appspot.com",
+    messagingSenderId: "758576304640",
+    appId: "1:758576304640:web:38c9ab697d338dd5407ee5"
+  }
 };
 
 /*
