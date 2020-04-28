4e421cf820d31a0d81957721ef24f033b7204a51
YuChih Hu <hijac.hu@gmail.com>
2020-04-28 16:34:36 +0800

    Refactor: Extract product-card component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { AdminAuthGuard } from './admin-auth.guard';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
 import { CategoryService } from './category.service';
 import { ProductService } from './product.service';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { ProductService } from './product.service';
     AdminOrdersComponent,
     LoginComponent,
     ProductFormComponent,
-    ProductFilterComponent
+    ProductFilterComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../modals/product';
+
+@Component({
+  selector: 'product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent {
+  // tslint:disable-next-line:no-input-rename
+  @Input('product') product: Product;
+  // tslint:disable-next-line:no-input-rename
+  @Input('show-actions') showActions = true;
+
+  constructor() { }
+
+
+}
